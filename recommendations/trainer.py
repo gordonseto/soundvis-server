644cb203486a9c9b7cfd13574c4a713c80d66aed
Gordon Seto <gordon.seto@gmail.com>
2018-04-03 16:26:03 -0700

    update trainer.py, took out genres

--- recommendations/trainer.py
+++ recommendations/trainer.py
@@ -91,16 +91,16 @@ for x in range(3*STRIDE, 4*STRIDE):
     create_user_with_ratings(x, ["Dance", "Drum and Bass", "Electronic", "House"])
 for x in range(4*STRIDE, 5*STRIDE):
     create_user_with_ratings(x, ["Oldies", "Old Time Radio", "60s", "70s"])
-for x in range(5 * STRIDE, 6 * STRIDE):
-    create_user_with_ratings(x, ["News", "Talk", "Comedy"])
-for x in range(6 * STRIDE, 7 * STRIDE):
-    create_user_with_ratings(x, ["Rock", "Classic Rock", "Electric Blues", "Hard Rock", "Blues"])
-for x in range(7 * STRIDE, 8 * STRIDE):
-    create_user_with_ratings(x, ["Folk", "Country", "Hot Country Hits"])
-for x in range(8 * STRIDE, 9 * STRIDE):
-    create_user_with_ratings(x, ["JPOP", "Japanese", "Asian"])
-for x in range(9 * STRIDE, 10 * STRIDE):
-    create_user_with_ratings(x, ["Classical", "Baroque"])
+# for x in range(5 * STRIDE, 6 * STRIDE):
+#     create_user_with_ratings(x, ["News", "Talk", "Comedy"])
+# for x in range(6 * STRIDE, 7 * STRIDE):
+#     create_user_with_ratings(x, ["Rock", "Classic Rock", "Electric Blues", "Hard Rock", "Blues"])
+# for x in range(7 * STRIDE, 8 * STRIDE):
+#     create_user_with_ratings(x, ["Folk", "Country", "Hot Country Hits"])
+# for x in range(8 * STRIDE, 9 * STRIDE):
+#     create_user_with_ratings(x, ["JPOP", "Japanese", "Asian"])
+# for x in range(9 * STRIDE, 10 * STRIDE):
+#     create_user_with_ratings(x, ["Classical", "Baroque"])
 for x in range(10 * STRIDE, 11 * STRIDE):
     create_user_with_ratings(x, ["Jazz", "Smooth Jazz"])
 for x in range(11 * STRIDE, 12 * STRIDE):
